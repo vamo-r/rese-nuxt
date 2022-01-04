042667e33f154fd2c0a7a7559f5e089f6e17120d
vamo-r <vamor.toda@gmail.com>
2022-01-04 10:33:46 +0900

    [add] 店舗検索コンポーネント追加

---
+++ components/SearchKeyword.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="search">
+    <input placeholder="店舗名検索"
+      v-if="$auth.loggedIn"
+      v-model="inputKeyword"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    keyword: String
+  },
+  computed: {
+    inputKeyword: {
+      get() {
+        return this.keyword
+      },
+      set(value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.search {
+  @include flex(center, center);
+  input {
+    padding: 5px 10px;
+    background: #fff;
+    border-radius: 5px;
+    font-size: 1.4rem;
+    color: #000;
+  }
+}
+</style>
