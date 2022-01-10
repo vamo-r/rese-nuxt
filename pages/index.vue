9da5565c80cde16799b40c309fce46ae0348c2a2
vamo-r <vamor.toda@gmail.com>
2022-01-10 23:51:48 +0900

    [update] トップページ更新

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,62 @@
 <template>
-  <Tutorial/>
+  <div class="wrap">
+    <Header
+      :keyword.sync="inputKeyword"
+    />
+    <main>
+      <div class="container">
+        <search-keyword v-model="inputKeyword"/>
+        <shop-card
+          :shops="filteredShops"
+        />
+      </div>
+    </main>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage'
-})
+<script>
+export default {
+  layout: 'noneHeader',
+  data() {
+    return {
+      shops: [],
+      keyword: ''
+    };
+  },
+  async created() {
+    await this.getShops();
+  },
+  computed: {
+    inputKeyword: {
+      get() {
+        return this.keyword
+      },
+      set(value) {
+        this.keyword = value
+      }
+    },
+    filteredShops() {
+      if (this.keyword !== '') {
+        return this.searchShopsName(this.shops, this.keyword);
+      }
+      return this.shops;
+    },
+  },
+  methods: {
+    async getShops() {
+      const response = await this.$axios.get('/shops');
+      this.shops = response.data.shops_data;
+    },
+    searchShopsName(shops, keyword) {
+      let filteredShops = [];
+      for (let i in shops) {
+        let shop = shops[i];
+        if (shop.name.indexOf(keyword) !== -1) {
+          filteredShops.push(shop);
+        }
+      }
+      return filteredShops;
+    }
+  }
+}
 </script>
