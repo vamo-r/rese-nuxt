2d8a588ac5bc7d1d39b96e0c8f43ba59c390ff15
vamo-r <vamor.toda@gmail.com>
2022-01-11 23:25:30 +0900

    [update] レイアウトcss追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,13 @@
 
 <style lang="scss">
 .app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
   background: #ccc;
+  main {
+    flex: 1;
+    overflow-x: hidden;
+  }
 }
 </style>
